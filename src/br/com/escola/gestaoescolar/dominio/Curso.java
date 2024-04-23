ec8f03567e97c7211e86f64975f5cb2fae9d3261
Fabiane Aguilera <fahernandes1974@gmail.com>
2024-04-23 17:26:59 -0300

    Alteracao nas classes junto c o Edy

--- src/br/com/escola/gestaoescolar/dominio/Curso.java
+++ src/br/com/escola/gestaoescolar/dominio/Curso.java
@@ -1,12 +1,35 @@
 package br.com.escola.gestaoescolar.dominio;
 
+import java.util.Objects;
+
 public class Curso {
+    private String codigo;
     private String nome;
     private int cargaHoraria;
+    private Nivel nivel;
 
-    public Curso(String nome, int cargaHoraria) {
+    public Curso(String codigo, String nome, int cargaHoraria, Nivel nivel) {
+        this.codigo = codigo;
         this.nome = nome;
         this.cargaHoraria = cargaHoraria;
+        this.nivel = nivel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Curso curso = (Curso) o;
+        return codigo.equals(curso.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+
+    public String getCodigo() {
+        return codigo;
     }
 
     public String getNome() {
@@ -15,5 +38,9 @@ public class Curso {
 
     public int getCargaHoraria() {
         return cargaHoraria;
+    }
+
+    public Nivel getNivel() {
+        return nivel;
     }
 }
